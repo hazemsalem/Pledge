f2444698c77290b2015866c7f3b640ea7fbe88e4
Hazem Fathy Salem Mahmoud <hazem_salem@hotmail.com>
2016-08-05 18:31:29 +1200

    Checkin theme

---
+++ app/Framework/WidgetContainer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'WidgetContainer',
+    templateUrl: 'fileName.component.html'
+})
+export class ComponentNameComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { 
+
+    }
+
+}
